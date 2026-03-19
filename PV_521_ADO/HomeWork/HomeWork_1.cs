20826879ca48a8cd5ae9f313e6cbe36f2f035aa9
Kegnar <mitriy.exe@gmail.com>
2026-03-19 08:32:54 +0400

    Добавлена папка для домашек. Сделано ДЗ№1

---
+++ PV_521_ADO/HomeWork/HomeWork_1.cs
@@ -0,0 +1,42 @@
+﻿using System.Data.SqlClient;
+using System;
+namespace PV_521_ADO.HomeWork
+
+
+{
+    public class HomeWork1
+    {
+        public static void PrintTable(string connectionString, string sqlCommand)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand command = new SqlCommand(sqlCommand, connection);
+
+            SqlDataReader reader = command.ExecuteReader();
+            for (int i = 0; i < reader.FieldCount; i++)
+                Console.Write(reader.GetName(i) + "\t");
+            Console.WriteLine();
+            while (reader.Read())
+            {
+                for (int i = 0; i < reader.FieldCount; i++)
+                    Console.Write($"{reader[i]}\t\t");
+                Console.WriteLine();
+            }
+
+            reader.Close();
+            connection.Close();
+        }
+        
+        public static string PrintScalar(string connectionString, string sqlCommand)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            connection.Open();
+            SqlCommand command = new SqlCommand(sqlCommand, connection);
+            string result = command.ExecuteScalar().ToString();
+            connection.Close();
+            return result;
+        }
+        
+    }
+}
--- PV_521_ADO/Program.cs
+++ PV_521_ADO/Program.cs
@@ -1,44 +1,60 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using System.Data.SqlClient;
+using PV_521_ADO.HomeWork;
 
 namespace PV_521_ADO
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			string connection_string = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Movies_PV_521;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-			Console.WriteLine(connection_string);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(connection_string);
+            string connectionString =
+                @"Data Source=MITRIY\TEST;
+                Initial Catalog=Movies_PV_521;
+                Integrated Security=True;
+                Connect Timeout=30;
+                Encrypt=False;
+                TrustServerCertificate=False;
+                ApplicationIntent=ReadWrite;
+                MultiSubnetFailover=False";
 
-			SqlConnection connection = new SqlConnection(connection_string);
-			connection.Open();
+            // SqlConnection connection = new SqlConnection(connectionString);
+            // connection.Open();
 
-			string cmd = 
-"SELECT movie_id,title,release_date,first_name,last_name FROM Movies,Directors WHERE director=director_id";
-			SqlCommand command = new SqlCommand(cmd, connection);
+            string cmd =
+                "SELECT movie_id,title,release_date,first_name,last_name FROM Movies,Directors WHERE director=director_id";
+            HomeWork1.PrintTable(connectionString, cmd);
+            
+            // SqlCommand command = new SqlCommand(cmd, connection);
+            //
+            // SqlDataReader reader = command.ExecuteReader();
+            // for (int i = 0; i < reader.FieldCount; i++)
+            //     Console.Write(reader.GetName(i) + "\t");
+            // Console.WriteLine();
+            // while (reader.Read())
+            // {
+            //     
+            //     for (int i = 0; i < reader.FieldCount; i++)
+            //         Console.Write($"{reader[i]}\t\t");
+            //     Console.WriteLine();
+            // }
+            //
+            // reader.Close();
 
-			SqlDataReader reader = command.ExecuteReader();
-			for (int i = 0; i < reader.FieldCount; i++)
-				Console.Write(reader.GetName(i) + "\t");
-			Console.WriteLine();
-			while (reader.Read())
-			{
-				//Console.WriteLine($"{reader[0]}\t{reader[1]}\t{reader[2]}\t{reader[3]}");
-				for (int i = 0; i < reader.FieldCount; i++)
-					Console.Write($"{reader[i]}\t\t");
-				Console.WriteLine();
-			}
-			reader.Close();
+            // command.CommandText = "SELECT COUNT(*) FROM Movies";
+            
+            string commandScalar = "SELECT COUNT(*) FROM Movies";
+            Console.WriteLine($"Количество записей:\t{HomeWork1.PrintScalar(connectionString, commandScalar)}");
 
-			command.CommandText = "SELECT COUNT(*) FROM Movies";
-			Console.WriteLine($"Количество записей:\t{command.ExecuteScalar()}");
+            Console.WriteLine();
+            // Console.WriteLine($"Количество записей:\t{command.ExecuteScalar()}");
+            //
+            //  connection.Close();
+        }
 
-			connection.Close();
-		}
-	}
+        
+
+    
+    }
 }
