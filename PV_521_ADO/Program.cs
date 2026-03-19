e08641a81d5d75ffbc275d1da7bb1ccc99ecd40a
Kegnar <mitriy.exe@gmail.com>
2026-03-19 08:34:04 +0400

    Подчистил main

--- PV_521_ADO/Program.cs
+++ PV_521_ADO/Program.cs
@@ -18,43 +18,15 @@ namespace PV_521_ADO
                 TrustServerCertificate=False;
                 ApplicationIntent=ReadWrite;
                 MultiSubnetFailover=False";
-
-            // SqlConnection connection = new SqlConnection(connectionString);
-            // connection.Open();
-
+            
             string cmd =
                 "SELECT movie_id,title,release_date,first_name,last_name FROM Movies,Directors WHERE director=director_id";
             HomeWork1.PrintTable(connectionString, cmd);
-            
-            // SqlCommand command = new SqlCommand(cmd, connection);
-            //
-            // SqlDataReader reader = command.ExecuteReader();
-            // for (int i = 0; i < reader.FieldCount; i++)
-            //     Console.Write(reader.GetName(i) + "\t");
-            // Console.WriteLine();
-            // while (reader.Read())
-            // {
-            //     
-            //     for (int i = 0; i < reader.FieldCount; i++)
-            //         Console.Write($"{reader[i]}\t\t");
-            //     Console.WriteLine();
-            // }
-            //
-            // reader.Close();
 
-            // command.CommandText = "SELECT COUNT(*) FROM Movies";
-            
             string commandScalar = "SELECT COUNT(*) FROM Movies";
             Console.WriteLine($"Количество записей:\t{HomeWork1.PrintScalar(connectionString, commandScalar)}");
 
             Console.WriteLine();
-            // Console.WriteLine($"Количество записей:\t{command.ExecuteScalar()}");
-            //
-            //  connection.Close();
         }
-
-        
-
-    
     }
 }
