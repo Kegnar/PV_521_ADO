8a8be7ad55891a4b621b0df1f396dd48087df612
Kegnar <mitriy.exe@gmail.com>
2026-03-20 20:28:25 +0400

    Допилил форматирование у таблицы

--- PV_521_ADO/HomeWork/HomeWork_1.cs
+++ PV_521_ADO/HomeWork/HomeWork_1.cs
@@ -32,7 +32,7 @@ namespace PV_521_ADO.HomeWork
             reader.Close();
             connection.Close();
         }
-        
+
 
         /// <summary>
         /// Возвращает строку - результат скалярного запроса
@@ -49,7 +49,12 @@ namespace PV_521_ADO.HomeWork
             connection.Close();
             return result;
         }
-        
+
+        /// <summary>
+        /// Возвращает результат SQL-запроса в виде форматированной таблицы
+        /// </summary>
+        /// <param name="connectionString">Строка-подключение к БД</param>
+        /// <param name="sqlCommand">SQL-запрос</param>
         public static void PrintAdvTable(string connectionString, string sqlCommand)
         {
             SqlConnection connection = new SqlConnection(connectionString);
@@ -58,17 +63,17 @@ namespace PV_521_ADO.HomeWork
 
             SqlDataReader reader = command.ExecuteReader();
             var tableSize = reader.FieldCount;
-            List<string[]> tableContents = new List<string[]>();  // результаты запроса
-            
-            string[] headers = new string[tableSize];               // заголовки столбцов
-            
-            int[] columnLength = new int[tableSize];                // массив с максимальными длинами столбцов
+            List<string[]> tableContents = new List<string[]>(); // результаты запроса
+
+            string[] headers = new string[tableSize]; // заголовки столбцов
+
+            int[] columnLength = new int[tableSize]; // массив с максимальными длинами столбцов
 
             for (int i = 0; i < tableSize; i++)
             {
                 headers[i] = reader.GetName(i);
             }
-            
+
             // Вычитываем результаты запроса в tableContents и одновременно собираем данные о максимальной длине строки в каждом столбце, с учетом длины заголовка столбца
             while (reader.Read())
             {
@@ -81,23 +86,25 @@ namespace PV_521_ADO.HomeWork
 
                 tableContents.Add(tmpColumn);
             }
+
             reader.Close();
             connection.Close();
-
+            
             for (int i = 0; i < tableSize; i++)
             {
-                Console.Write($"{headers[i].PadRight(columnLength[i]+1)}");
+                Console.Write($"{headers[i].PadRight(columnLength[i] + 1)}");
             }
-            Console.WriteLine();
+
+            Console.WriteLine('\n');
             foreach (var str in tableContents)
             {
                 for (int i = 0; i < tableSize; i++)
                 {
-                Console.Write($"{str[i].PadRight(columnLength[i]+1)}");
+                    Console.Write($"{str[i].PadRight(columnLength[i] + 1)}");
                 }
+
                 Console.WriteLine();
             }
-            
         }
     }
 }
--- PV_521_ADO/Program.cs
+++ PV_521_ADO/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Data.SqlClient;
 using PV_521_ADO.HomeWork;
 
 namespace PV_521_ADO
@@ -19,12 +18,11 @@ namespace PV_521_ADO
                 ApplicationIntent=ReadWrite;
                 MultiSubnetFailover=False";
             
-            string cmd =
-                "SELECT movie_id,title,release_date,first_name,last_name FROM Movies,Directors WHERE director=director_id";
+            string cmd = "SELECT movie_id,title,release_date,first_name,last_name FROM Movies,Directors WHERE director=director_id";
             HomeWork1.PrintAdvTable(connectionString, cmd);
 
             string commandScalar = "SELECT COUNT(*) FROM Movies";
-            //Console.WriteLine($"Количество записей:\t{HomeWork1.PrintScalar(connectionString, commandScalar)}");
+            Console.WriteLine($"Количество записей:\t{HomeWork1.PrintScalar(connectionString, commandScalar)}");
 
             Console.WriteLine();
         }
