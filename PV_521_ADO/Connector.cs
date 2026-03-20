4dd183fc23666b7fa206882998bf75ac6cf619b9
Kegnar <mitriy.exe@gmail.com>
2026-03-20 22:32:52 +0400

    продолжаем пилить Connector

--- PV_521_ADO/Connector.cs
+++ PV_521_ADO/Connector.cs
@@ -1,28 +1,29 @@
 ﻿using System;
 
 namespace PV_521_ADO;
+
 using System.Data.SqlClient;
 
 public class Connector
 {
     string connectionString;
     SqlConnection connection;
+
     public Connector(string connectionString)
     {
         Console.WriteLine(connectionString);
         this.connectionString = connectionString;
         connection = new SqlConnection(connectionString);
     }
-    
+
     public void Select(string cmd)
     {
-            
         connection.Open();
         SqlCommand command = new SqlCommand(cmd, connection);
         SqlDataReader reader = command.ExecuteReader();
         for (int i = 0; i < reader.FieldCount; i++)
         {
-            Console.WriteLine(reader.GetName(i) + 't');
+            Console.Write(reader.GetName(i) + '\t');
         }
 
         Console.WriteLine();
@@ -31,10 +32,19 @@ public class Connector
             for (int i = 0; i < reader.FieldCount; i++)
             {
                 Console.Write($"{reader[i]}\t\t");
-                Console.WriteLine();
             }
+
+            Console.WriteLine();
         }
+
         connection.Close();
+    }
+
+    public void Select(string fields, string tables, string conditions = null)
+    {
+        string cmd = $"SELECT {fields} FROM {tables}";
+        if (conditions != null) cmd += $" WHERE {conditions}";
+        Select(cmd);
     }
 
     public object Scalar(string cmd)
@@ -43,8 +53,43 @@ public class Connector
         object result = null;
         SqlCommand command = new SqlCommand(cmd, connection);
         result = command.ExecuteScalar();
-                
+
         connection.Close();
         return result;
+    }
+
+    public void Insert(string cmd)
+    {
+        SqlCommand command = new SqlCommand(cmd, connection);
+        connection.Open();
+
+        try
+        {
+            command.ExecuteNonQuery();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.GetType());
+            Console.WriteLine(e.Message);
+           
+        }
+        connection.Close();
+    }
+
+    public int GetMaxPrimaryKey(string table)
+    {
+        string cmd = $"Select * from {table}";
+        SqlCommand command = new SqlCommand(cmd, connection);
+        connection.Open();
+        SqlDataReader reader = command.ExecuteReader();
+        string pkName = reader.GetName(0);
+        reader.Close();
+        connection.Close();
+        return (int)Scalar($"SELECT MAX({pkName}) FROM {table}");
+    }
+
+    public int GetNextPrimaryKey(string table)
+    {
+        return GetMaxPrimaryKey(table) + 1;
     }
 }
--- PV_521_ADO/Program.cs
+++ PV_521_ADO/Program.cs
@@ -21,17 +21,25 @@ namespace PV_521_ADO
                 MultiSubnetFailover=False";
 
             Connector connector = new Connector(connectionString);
+            
+            connector.Insert("INSERT Directors (first_name, last_name) VALUES (N'Guy', N'Richie');");
+            connector.Select("*","Directors");
+            connector.Select(
+                "title, release_date, first_name, last_name",
+                "Movies, Directors",
+                "director = director_id");
+            Console.WriteLine($"PK Max: {connector.GetMaxPrimaryKey("Directors")}");
 
-            string cmd =
-                "SELECT movie_id,title,release_date,first_name,last_name FROM Movies,Directors WHERE director=director_id";
-            HomeWork1.PrintAdvTable(connectionString, cmd);
-
-            string commandScalar = "SELECT COUNT(*) FROM Movies";
-            Console.WriteLine($"Количество записей:\t{HomeWork1.PrintScalar(connectionString, commandScalar)}");
-
-            Console.WriteLine();
-            connector.Select("SELECT COUNT(*) FROM Movies");
-            Console.WriteLine($"Количество записей: {connector.Scalar("SELECT COUNT(*) FROM Directors")}");
+            // string cmd =
+            //     "SELECT movie_id,title,release_date,first_name,last_name FROM Movies,Directors WHERE director=director_id";
+            // HomeWork1.PrintAdvTable(connectionString, cmd);
+            //
+            // string commandScalar = "SELECT COUNT(*) FROM Movies";
+            // Console.WriteLine($"Количество записей:\t{HomeWork1.PrintScalar(connectionString, commandScalar)}");
+            //
+            // Console.WriteLine();
+            // connector.Select("SELECT COUNT(*) FROM Movies");
+            // Console.WriteLine($"Количество записей: {connector.Scalar("SELECT COUNT(*) FROM Directors")}");
         }
     }
 }
